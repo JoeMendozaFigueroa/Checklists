264e448a2bca41d92ef1dc8df6c440b9dfcaca7d
JoeMendoza <joemendoza411@gmail.com>
2021-09-05 22:34:52 -0400

    update

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -21,11 +21,6 @@ class AllListsViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
